839885d90e87fc89ef8685ba2f053cb9a71ca695
mercy-mwai <mercyneemam@gmail.com>
2023-06-21 06:13:41 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: The character to be checked
+ * Return: 1 for alphabetic characters, 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+}
