2f4da904c5a709adb78dff2769b394dff8cb2df6
mercy-mwai <mercyneemam@gmail.com>
2023-07-26 00:46:32 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts - Entry point- prints a string
+* @str: the string input
+*
+* Return: returns the length of a string
+*/
+int _puts(char *str)
+{
+int x = 0;
+
+while (str[x] != '\0')
+{
+_putchar(str[x]);
+x++;
+}
+return (x);
+_putchar('\n');
+}
