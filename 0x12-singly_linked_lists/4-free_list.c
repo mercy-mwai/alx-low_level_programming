3adf37815ec37f35e0ea8edaa823a162e1dbd4d5
mercy-mwai <mercyneemam@gmail.com>
2023-07-27 08:29:00 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a list_t list
+ * @head: first node
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
