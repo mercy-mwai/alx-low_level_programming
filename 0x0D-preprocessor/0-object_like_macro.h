70848d8ed0096c2957d974b33f24f349f0137e49
mercy-mwai <133628888+mercy-mwai@users.noreply.github.com>
2023-08-05 09:37:39 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef SIZE
-#define SIZE 1024
-
-#endif /* 0-create_like_macro.h */
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) ((x) < (0) ? -(x) : (x))
-
-#endif /* ifndef 3-function_like_macro.h */
